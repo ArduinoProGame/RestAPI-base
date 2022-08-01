b6f9528c3b9dfc7a317b19f81417f840266b604c
Pablo Estrella <pablolili5370@gmail.com>
2022-08-01 17:36:21 -0500

    README added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Web Server + REST server
+
+Recuerden que deben ejecutar ```npm install```para reconstruir
+los modulos de node
--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//Para recuperar mi proyecto de Git si se borro algo puedo hacer con
+//el siguiente comando: git checkout -- .
+//https://www.udemy.com/course/node-de-cero-a-experto/learn/lecture/9608776#overview
+
 require('dotenv').config();
 
 const Server = require('./models/server.js');
